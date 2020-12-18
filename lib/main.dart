269ad4fe8a06a11d344b8c5b126c0887524ad5d3
kadielif <elif_kadi@hotmail.com>
2020-12-18 19:31:20 +0300

    animated opacity added for restart button

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,15 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   static const duration = const Duration(seconds: 1);
-
   List<int> val = Values.getValue();
   List<int> colorVal;
-
   bool isFinish = false;
-
   int secondPassed = 0;
-  int seconds, minutes, hours;
-  int maxScore, first;
-
+  int bestScore, first;
   Timer timer;
-
   String time = "00 : 00 : 00";
   String timerText = "";
+  double opacityLevel = 0.0;
 
   @override
   void initState() {
@@ -40,14 +35,13 @@ class _MyAppState extends State<MyApp> {
   void handleTick() {
     if (!isFinish) {
       setState(() {
+        opacityLevel = 0.0;
         secondPassed++;
       });
     } else {
       setState(() {
+        opacityLevel = 1.0;
         first = 1;
-        seconds = 0;
-        minutes = 0;
-        hours = 0;
       });
     }
   }
@@ -59,25 +53,31 @@ class _MyAppState extends State<MyApp> {
         handleTick();
       });
     }
-    time = cal(secondPassed);
+    time = timerTextParse(secondPassed);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Schulte Table Game"),
+          title: Text(
+            "Schulte Table Game",
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
           backgroundColor: Colors.redAccent.shade200,
         ),
         body: Padding(
           padding: const EdgeInsets.fromLTRB(20.0, 8.0, 8.0, 8.0),
           child: Column(
             children: [
+              SizedBox(
+                height: 80,
+              ),
               Text(
                 "Best   : " + timerText,
-                style: (Theme.of(context).textTheme.headline5),
+                style: (Theme.of(context).textTheme.headline4),
               ),
               Text(
                 "Timer : " + time,
-                style: (Theme.of(context).textTheme.headline5),
+                style: (Theme.of(context).textTheme.headline4),
               ),
             ],
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -86,7 +86,7 @@ class _MyAppState extends State<MyApp> {
         floatingActionButton: GridView.count(
           crossAxisCount: 5,
           crossAxisSpacing: 1,
-          padding: EdgeInsets.fromLTRB(30.0, 280.0, 0.0, 50.0),
+          padding: EdgeInsets.fromLTRB(30.0, 350.0, 0.0, 50.0),
           children: List.generate(
             25,
             (index) {
@@ -102,35 +102,35 @@ class _MyAppState extends State<MyApp> {
                   color: colorVal[index] == 2
                       ? Colors.green.shade300
                       : Colors.grey.shade300,
-                  onPressed: () => control(index),
+                  onPressed: () => indexControllers(index),
                 ),
               );
             },
           ),
         ),
-        bottomNavigationBar: Container(
-          padding: EdgeInsets.all(5),
-          child: RaisedButton(
-            child: Text(
-              "Restart",
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: AnimatedOpacity(
+            opacity: opacityLevel,
+            duration: Duration(milliseconds: 100),
+            child: RaisedButton(
+              child: Text(
+                "RESTART",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+              onPressed: () => restartButton(),
+              color: Colors.redAccent.shade200,
             ),
-            textColor: Colors.white,
-            onPressed: () {
-              setState(() {
-                secondPassed = 0;
-                val = Values.getValue();
-                colorVal = Values.getColorValue();
-                isFinish = false;
-              });
-            },
-            color: Colors.redAccent.shade200,
           ),
         ),
       ),
     );
   }
 
-  control(int index) {
+  indexControllers(int index) {
     if (val[index] == first) {
       setState(() {
         colorVal[index] = 2;
@@ -138,13 +138,12 @@ class _MyAppState extends State<MyApp> {
       });
       if (val[index] == 25) {
         setState(() {
-          first = 0;
-          if (maxScore == null) {
-            maxScore = secondPassed;
-            timerText = cal(maxScore);
-          } else if (secondPassed < maxScore) {
-            maxScore = secondPassed;
-            timerText = cal(maxScore);
+          if (bestScore == null) {
+            bestScore = secondPassed;
+            timerText = timerTextParse(bestScore);
+          } else if (secondPassed < bestScore) {
+            bestScore = secondPassed;
+            timerText = timerTextParse(bestScore);
           }
           isFinish = true;
         });
@@ -152,8 +151,8 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  //maxScore u text e çevirir.
-  cal(int secondPassed) {
+  timerTextParse(int secondPassed) {
+    int seconds, minutes, hours;
     String timeText;
     seconds = secondPassed % 60;
     minutes = secondPassed ~/ 60;
@@ -165,5 +164,14 @@ class _MyAppState extends State<MyApp> {
         " : " +
         seconds.toString().padLeft(2, '0');
     return timeText;
+  }
+
+  restartButton() {
+    setState(() {
+      secondPassed = 0;
+      val = Values.getValue();
+      colorVal = Values.getColorValue();
+      isFinish = false;
+    });
   }
 }
--- lib/values.dart
+++ lib/values.dart
@@ -1,28 +1,21 @@
 import 'dart:math';
 
 class Values {
-  static List<int> values = List<int>();
-  static List<int> colorValues = List<int>();
   static List<int> getValue() {
-    // List tempValues;
+    List<int> values = List<int>();
+    var rng = new Random();
     if (values.length == 25) {
       values = new List<int>();
     }
-    var rng = new Random();
-    values.add(rng.nextInt(25));
-
-    for (int i = 1; i < 25; i++) {
+    for (int i = 0; i < 25; i++) {
       var x = rng.nextInt(25);
-      if (values.contains(x + 1) == false) {
-        values.add(x + 1);
-      } else {
-        i--;
-      }
+      !values.contains(x + 1) ? values.add(x + 1) : i--;
     }
     return values;
   }
 
   static List<int> getColorValue() {
+    List<int> colorValues = List<int>();
     if (colorValues.length == 25) {
       colorValues = new List<int>();
     }
